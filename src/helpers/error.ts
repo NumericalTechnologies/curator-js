fd8da88a5a4b9b6ecfbdd56cbd9008949b1e79be
Prakoso Santoso <prakoso.santoso@numtech.com>
2024-09-30 10:30:05 +0800

    Update error handling

--- src/helpers/error.ts
+++ src/helpers/error.ts
@@ -1,8 +1,27 @@
-export function parseZooKeeperError(error: Error) {
-  const errorMessage = error.message;
-  const seperatorIndex = errorMessage.indexOf(" ");
-  return {
-    code: Number.parseInt(errorMessage.substring(0, seperatorIndex), 10),
-    message: errorMessage.substring(seperatorIndex + 1),
-  };
+import { Exception } from "node-zookeeper-client";
+
+export function parseZooKeeperError(error: unknown) {
+  if (error instanceof Exception) {
+    // The 'message' property is not typed but actually exists.
+    // See: https://github.com/alexguan/node-zookeeper-client/blob/eec7a566a2e9ce7f148f3092d677054bb5b1d345/lib/Exception.js#L66
+    return {
+      code: error.code,
+      message:
+        "message" in error && typeof error.message === "string"
+          ? error.message
+          : "",
+    };
+  } else if (error instanceof Error) {
+    const errorMessage = error.message;
+    const seperatorIndex = errorMessage.indexOf(" ");
+    return {
+      code: Number.parseInt(errorMessage.substring(0, seperatorIndex), 10),
+      message: errorMessage.substring(seperatorIndex + 1),
+    };
+  } else {
+    return {
+      code: NaN,
+      message: "",
+    };
+  }
 }
